fec4ff83d5c7070ae35be0787a4bc6d6aaf85471
deepakhybrow <deepak@mail.hybrowlabs.com>
2026-05-18 14:34:48 +0530

    pdf viewer fix

--- app/job_offer/PdfViewer.tsx
+++ app/job_offer/PdfViewer.tsx
@@ -7,7 +7,7 @@ import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
 // Configure the worker
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+pdfjs.GlobalWorkerOptions.workerSrc = "/pdf.worker.min.js";
 
 interface PdfViewerProps {
   pdfUrl: string;
